d5f88f02377028a2482ad46758a18c54ec32a08f
ShahabAthar25 <shahabathar25@gmail.com>
2021-11-18 09:03:21 +0500

    Add Mongodb Schema

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/api",
